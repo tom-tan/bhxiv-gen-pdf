96774c377c32f0c46c4f1ff32a55783eab42c55a
Pjotr Prins <pjotr.public01@thebird.nl>
2020-05-02 08:18:38 -0500

    Web paths

--- web/pdf-web-gen.rkt
+++ web/pdf-web-gen.rkt
@@ -22,7 +22,10 @@
            (section ((class "page-body"))
                     (h1 ,title)
                     ,(include-template/xml "templates/preview.html"))
-           (hr)
+           (section ((class "page-footer"))
+                    (hr)
+                    (div ((class "copyright"))
+                         (div "by the BioHackrXiv team")))
            ))))
 
    ; (response/xexpr
@@ -41,6 +44,7 @@
 
 (define-values (dispatch generate-url)
   (dispatch-rules
+    [("") start]
     [("preview") start]
     [("test") preview]
     [else (error "There is no procedure to handle the url.")]))
@@ -51,8 +55,7 @@
 (serve/servlet request-handler
                #:port 8080
                #:launch-browser? #t
-               #:servlet-path "/preview"
-               #:servlet-regexp #rx"start|review"
+               #:servlet-path "/"
                #:server-root-path (current-directory)
                #:extra-files-paths
                (list
